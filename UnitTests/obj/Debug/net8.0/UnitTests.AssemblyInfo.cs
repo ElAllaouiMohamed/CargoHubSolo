72c4610818f8fa33dd2843417c4456e98ff7090a
ElAllaouiMohamed <miemiet012@gmail.com>
2025-05-27 23:56:26 +0200

    coveage

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a038bbb2e6a972d4db7857f17305014879ec20b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1a202940ad077fcc75c380793f23bbc043e3e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
