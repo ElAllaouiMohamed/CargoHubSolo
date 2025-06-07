13013e37c0d97aab70b951c7bfe5cda7f0f23bdd
ElAllaouiMohamed <miemiet012@gmail.com>
2025-06-07 20:32:22 +0200

    code geformat c#

--- CargoHubV2/Models/Client.cs
+++ CargoHubV2/Models/Client.cs
@@ -1,4 +1,4 @@
-﻿using CargohubV2.DataConverters;
+using CargohubV2.DataConverters;
 using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
--- CargoHubV2/obj/Debug/net8.0/CargoHubV2.AssemblyInfo.cs
+++ CargoHubV2/obj/Debug/net8.0/CargoHubV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62650b0bce4990bcd139168553e20d08cc26563d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d0e576281d13d514c5ce18d5506577abf1653")]
 [assembly: System.Reflection.AssemblyProductAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
