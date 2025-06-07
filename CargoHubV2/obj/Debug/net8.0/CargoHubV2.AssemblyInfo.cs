2081da792cabda7ae24b714f0e5b4d740547b241
ElAllaouiMohamed <miemiet012@gmail.com>
2025-06-07 05:31:02 +0200

    pipeline test

--- CargoHubV2/obj/Debug/net8.0/CargoHubV2.AssemblyInfo.cs
+++ CargoHubV2/obj/Debug/net8.0/CargoHubV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e716f43da998172d0a823ca58c7196d48af8877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4ca6b66e119d168050c224710ff9214c84c339")]
 [assembly: System.Reflection.AssemblyProductAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CargoHubV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
