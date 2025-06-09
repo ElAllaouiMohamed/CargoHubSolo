1232b20d7e052fa6dc5e15599aa0436bcd3a7c7a
ElAllaouiMohamed <139189815+ElAllaouiMohamed@users.noreply.github.com>
2025-06-09 19:04:12 +0200

    integratie en validatie voor supplier endpoint (#15)
    * pipeline aangepast voor branch
    
    * integratie testen en validatie voor supplier
    
    * order integration test aangepast basis van nieuwe model supplier

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009a779e57a9419ea6791629abf8999c39e0699a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783d0b6dd3d634583b2aed7b74af0bb8ab44bda5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
